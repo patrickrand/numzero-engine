c3757e7464267184c8f13c71912975adc9389169
patrickrand <prand0001@gmail.com>
2016-10-30 20:51:01 -0400

    initial commit

---
+++ game/game.go
@@ -0,0 +1,22 @@
+package game
+
+type Game struct {
+	ID           int           `json:"id"`
+	Achievements []Achievement `json:"achievements"`
+}
+
+type Achievement struct {
+	ID    int    `json:"id"`
+	Rules []Rule `json:"rules"`
+}
+
+type Rule struct {
+	Name string                 `json:"id"`
+	Type string                 `json:"type"`
+	Args map[string]interface{} `json:"args"`
+}
+
+type Fact struct {
+	Name  string `json:"name"`
+	Value int    `json:"value"`
+}
