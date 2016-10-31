38c72af1d4fd46ec546edc1a796ecbae0b2c1d7c
patrickrand <prand0001@gmail.com>
2016-10-30 23:04:54 -0400

    updated data structs

--- game/game.go
+++ game/game.go
@@ -1,22 +1,26 @@
 package game
 
 type Game struct {
-	ID           int           `json:"id"`
-	Achievements []Achievement `json:"achievements"`
+	ID           int                 `json:"id"`
+	Achievements map[int]Achievement `json:"achievements"`
+	Players      map[string]Player   `json:"players"`
+}
+
+type Player struct {
+	ID       int                    `json:"id"`
+	Progress map[string]interface{} `json:"progress"` // map[Achievement.ID]interface{}
 }
 
 type Achievement struct {
-	ID    int    `json:"id"`
-	Rules []Rule `json:"rules"`
+	ID    int          `json:"id"`
+	Rules []Rule       `json:"rules"`
+	Facts map[Fact]int `json:"facts"` // built via parsing Rules
 }
 
 type Rule struct {
 	ID   int                    `json:"id"`
-	Type string                 `json:"type"`
-	Args map[string]interface{} `json:"args"`
+	Type string                 `json:"type"` // i.e. "count"
+	Args map[string]interface{} `json:"args"` // map["fact"]:Fact, map["total"]:int
 }
 
-type Fact struct {
-	Name  string `json:"name"`
-	Value int    `json:"value"`
-}
+type Fact string
